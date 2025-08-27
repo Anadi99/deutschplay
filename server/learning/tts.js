fd1296edb5af1645308151accc88599e16b409a7
Anadi99 <anadimishra973@gmail.com>
2025-08-27 19:32:23 +0530

    Added learning enhancements: sentence validation and TTS endpoint

--- server/learning/tts.js
+++ server/learning/tts.js
@@ -2,7 +2,7 @@ const textToSpeech = require('gtts');
 const express = require('express');
 const router = express.Router();
 router.get('/tts/:word', (req,res)=>{
-  const gtts = new textToSpeech(req.params.word, 'de');
-  gtts.save('speech.mp3', ()=>res.send('Done'));
+  const gtts = new textToSpeech(req.params.word,'de');
+  gtts.save('speech.mp3',()=>res.send('Done'));
 });
 module.exports = router;
